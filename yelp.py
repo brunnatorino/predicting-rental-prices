927cc57794cdbc4841dd9950878b890c67c41d57
Brunna Torino <44169374+brunnatorino@users.noreply.github.com>
2020-04-30 01:45:52 +0200

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -2,14 +2,21 @@ import requests
 import json
 
 
-api_key='secret'
+api_key='your-api-key-here'
 headers = {'Authorization': 'Bearer %s' % api_key}
-
 url='https://api.yelp.com/v3/businesses/search'
 
+## creating global empty lists so we don't overwrite them but keep adding data to them
+rating = []
+zipcode = []
+cities2 = []
+prices = []
+
+## offset can be explained as the page number and with limit can gather max 1000 business data from Yelp every day
 offset = 1
-while count <= 901:
-    params={'term':'Restaurants', 'location': 'amsterdam', 'limit': 1, 'offset': offset}
+## loop to iterate over 7 pages of 50 businesses each = 350 businesses in Amsterdam 
+while offset <= 7:
+    params={'term':'Restaurants', 'location': 'amsterdam', 'limit': 50, 'offset': offset}
     req = requests.get(url, params=params, headers=headers)
     parsed = json.loads(req.text)
     n = 0
@@ -18,30 +25,13 @@ while count <= 901:
             price_data = parsed["businesses"][n]['price']
             ratings_data = parsed["businesses"][n]['rating']
             zipcode_data = parsed["businesses"][n]["location"]["zip_code"]
+            
             rating.append(ratings_data)
             zipcode.append(zipcode_data)
-            cities2.append(cities_data)
             prices.append(price_data)
-        except KeyError:
+            
+        except:
+            ## some of the data gathered are not going to have the necessary information
+            ## so we skip those 
             pass
-        n += 1
-    offset += 50
-
-
-yelp1 = pd.DataFrame({'name':name, 'rating':rating, 'address':address,'zipcode':zipcode, 'city':city, 'prices':prices,'coordinates':coordinates})
-
-yelp1['zip_len'] = yelp1.zipcode.str.len()
-yelp1 = yelp1[yelp1['zip_len'] > 4]
-yelp = yelp1.copy()
-yelp['postcode2'] = yelp['zipcode'].str.replace('\s','')
-yelp['postcode2']= yelp.postcode2.str[0:5]
-yelp['price_len'] = yelp.prices.str.len()
-yelp_prices = yelp.groupby(['postcode2']).price_len.mean()
-yelp_rate = yelp.groupby(['postcode2']).rating.mean()
-yelp_prices = pd.DataFrame(data=yelp_prices)
-yelp_rate = pd.DataFrame(data=yelp_rate)
-dict1 = yelp_prices.to_dict()['price_len']
-dict2 = yelp_rate.to_dict()['rating']
-
-amsterdam = df.copy()
-
+    offset += 1
